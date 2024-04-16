7210fa3c9d11a004b010cb83fc56a5dd0df36f8e
MarekL-MarekL <markandmark3@gmail.com>
2024-04-16 16:35:34 +0200

    branches

--- JokesWebApp/Models/Joke.cs
+++ JokesWebApp/Models/Joke.cs
@@ -3,6 +3,7 @@
     public class Joke
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public string JokeQuestion { get; set; }
 
         public string JokeAnswer { get; set; }  
