7991d6e6e2cc683092c4f533409d88831ce84694
MarekL-MarekL <markandmark3@gmail.com>
2024-04-16 15:28:52 +0200

    My first change ML

--- JokesWebApp/Controllers/JokesController.cs
+++ JokesWebApp/Controllers/JokesController.cs
@@ -62,7 +62,7 @@ namespace JokesWebApp.Controllers
 
         // GET: Jokes/Create
 
-        [Authorize]
+        
         public IActionResult Create()
         {
             return View();
@@ -72,7 +72,7 @@ namespace JokesWebApp.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
 
-        [Authorize]
+       
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
@@ -88,7 +88,7 @@ namespace JokesWebApp.Controllers
 
         // GET: Jokes/Edit/5
 
-        [Authorize]
+      
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -108,7 +108,7 @@ namespace JokesWebApp.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
 
-        [Authorize]
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,JokeQuestion,JokeAnswer")] Joke joke)
@@ -142,7 +142,7 @@ namespace JokesWebApp.Controllers
         }
 
 
-        [Authorize]
+      
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -162,7 +162,7 @@ namespace JokesWebApp.Controllers
 
         // POST: Jokes/Delete/5
 
-        [Authorize]
+       
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
