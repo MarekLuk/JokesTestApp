9e7c9abbb07b5dc87cfcb234fe5f942e461c4a9f
MarekL-MarekL <markandmark3@gmail.com>
2024-04-16 16:08:04 +0200

    check conflict

--- JokesWebApp/Sol/Sols.cs
+++ JokesWebApp/Sol/Sols.cs
@@ -2,8 +2,10 @@
 {
     public class Sols
     {
-<<<<<<< HEAD
+
+        //check conflict
         public int SomeInt { get; set; }
+        //check conflict
 =======
         public LocalDataStoreSlot LocalDataStoreSlot { get; set; }
 >>>>>>> e339630bee6b470ff4d1e6b54b1fb4f5f053e620
